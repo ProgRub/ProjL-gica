992a3770daefc2004dedc20cd69c9281ef77475d
Diego Briceño <diegobriceno13@gmail.com>
2019-12-04 09:41:46 +0000

    Lista Binaria

--- Projeto.pl
+++ Projeto.pl
@@ -1,1 +1,5 @@
-/*Projeto Lógica*/
+/*Projeto Lógica
+
+lista_n_0s_e_1s(0,[]).
+lista_n_0s_e_1s(N,[0|L]):- N>0, N1 is N-1, lista_n_0s_e_1s(N1,L).
+lista_n_0s_e_1s(N,[1|L]):- N>0, N1 is N-1, lista_n_0s_e_1s(N1,L).
