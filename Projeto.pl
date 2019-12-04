d6adac0fd4bc736ffbca47607ffd6a5a7675cc22
Diego Briceño <diegobriceno13@gmail.com>
2019-12-04 14:50:33 +0000

    Membro

--- Projeto.pl
+++ Projeto.pl
@@ -6,15 +6,23 @@
 :-op(300, xfy, 'ou').
 :-op(400, xfy, 'imp').
 
+
+/* membro/2 é tal que membro(X,L) tem o valor verdadeiro se X pertence à lista L*/
+membro(X, [X | _]).
+membro(X, [_ | C]):-membro(X, C).
+
+
 /* comprimento/2 é tal que comprimento(L,N) é verdadeiro se N é o 
 comprimento da lista L*/
 comprimento([], 0).
 comprimento([A|X], N):-comprimento(X, N1), N is N1+1.
 
+
 /* enesimo/3 é tal que enesimo(N,L,X) é verdadeiro se N é um número
 natural positivo e X é o elemento que está na posição N da lista L*/
 enesimo(1,[X|L],X).
 enesimo(N,[X|L],Y):-enesimo(N1,L,Y), N is N1+1.
+
 
 /* calc_valor/4 é tal que ao executar uma consulta do tipo 
 calc_valor(F,S,L,V) onde F é uma fórmula da Linguagem neg, e, ou e imp,
@@ -29,6 +37,7 @@ calc_valor(neg X,L1,L2,1):- calc_valor(X,L1,L2,0).
 calc_valor(X imp Y,L1,L2,0):- calc_valor(X,L1,L2,1), calc_valor(Y,L1,L2,0).
 calc_valor(X imp Y,L1,L2,1):- not(calc_valor(X imp Y,L1,L2,0)).
 
+
 /* lista_n_0s_e_1s/2 é tal que lista_n_0s_e_1s(N,L) é verdadeiro se
 L é uma lista constituída apenas por 0s e 1s de comprimento N, sendo
 N um número inteiro não negativo*/
@@ -40,4 +49,4 @@ lista_n_0s_e_1s(N,[1|L]):- N>0, N1 is N-1, lista_n_0s_e_1s(N1,L).
 todas_listas_0s_1s(N):- findall(L,list_n_0s_e_1s(N,L),R),write(R).
 
 
-simb_prop(F):- not(F= neg X), not(F= X e Y), not(F = X ou Y).
+simb_prop(F):- not(F= neg X), not(F= X e Y), not(F = X ou Y), not(F = X imp Y).
